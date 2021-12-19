cc0b7d8eb29eaaed3774bded5270a68f90e85c0b
daniele <mucci.daniele@gmail.com>
2021-12-19 12:36:25 +0200

    first commit

---
+++ main.go
@@ -0,0 +1,134 @@
+package main
+
+import (
+	"errors"
+	"flag"
+	"os"
+	"text/template"
+	"time"
+
+	log "github.com/sirupsen/logrus"
+	"github.com/sudneo/csv2beancount/models"
+	"github.com/sudneo/csv2beancount/parsers"
+	"github.com/sudneo/csv2beancount/utils"
+)
+
+func init() {
+	log.SetFormatter(&log.TextFormatter{})
+	log.SetOutput(os.Stdout)
+	log.SetLevel(log.InfoLevel)
+}
+
+func ProcessTransactions(t []models.Transaction, defaultAccount models.Account, mappings models.Mappings) []models.Transaction {
+	var processedTransactions []models.Transaction
+	for _, transaction := range t {
+		match, desc := transaction.Categorize(mappings)
+		var matchAccount models.Account
+		if match != "" {
+			matchAccount.Name = match
+		} else {
+			matchAccount.Name = "UNCATEGORIZED_ACCOUNT"
+		}
+		processed := transaction
+		if desc != "" {
+			processed.Description = desc
+		}
+		if transaction.Type == "debit" {
+			processed.DebitAccount = defaultAccount
+			processed.CreditAccount = matchAccount
+		} else {
+			processed.DebitAccount = matchAccount
+			processed.CreditAccount = defaultAccount
+		}
+		processedTransactions = append(processedTransactions, processed)
+	}
+	return processedTransactions
+}
+
+func FilterTransactionsByDate(startDate time.Time, endDate time.Time, transactions []models.Transaction) []models.Transaction {
+	var t []models.Transaction
+	for _, transaction := range transactions {
+		if (transaction.Date.After(startDate) || transaction.Date.Equal(startDate)) && (transaction.Date.Before(endDate) || transaction.Date.Equal(endDate)) {
+			t = append(t, transaction)
+		}
+	}
+	return t
+}
+
+func GenerateOutput(outfile string, transactions []models.Transaction) error {
+	PageData := `{{ range . }}
+{{ .Date.Format "2006-01-02" }} * "{{ .Description }}"
+	{{ .CreditAccount.Name }} {{ .Amount }} {{ .Currency }}
+	{{ .DebitAccount.Name }} -{{ .Amount }} {{ .Currency }} 
+{{ end }}
+`
+
+	t := template.Must(template.New("Config").Parse(PageData))
+
+	f, err := os.Create(outfile)
+	if err != nil {
+		log.WithFields(log.Fields{
+			"File":  outfile,
+			"Error": err,
+		}).Error("Failed to create output file")
+		return errors.New("Failed to render template: cannot create the output file")
+	}
+	t.Execute(f, transactions)
+	return nil
+}
+
+func Run(config models.Config, defaultAccount models.Account) error {
+	accounts := utils.GetAccounts(config.BeancountLedger)
+	log.WithFields(log.Fields{
+		"Accounts": len(accounts),
+	}).Info("Found beancount accounts")
+	m, err := models.ReadMapping(config.MappingFile)
+	if err != nil {
+		return err
+	}
+	m.ValidateMappings(accounts)
+	log.Info("Mappings validated")
+	transactions, err := parsers.ParseCSV(config.CSVFile, config.CSVType)
+	if err != nil {
+		return err
+	}
+	log.WithFields(log.Fields{
+		"Transactions": len(transactions),
+	}).Info("Found and parsed transactions")
+	if config.StartDate != "" && config.EndDate != "" {
+		transactions = FilterTransactionsByDate(config.InternalStartDate, config.InternalEndDate, transactions)
+		if len(transactions) == 0 {
+			log.Warning("The Start/End date specified in the configuration file are filtering out all the transactions.")
+		}
+	}
+	transactions = ProcessTransactions(transactions, defaultAccount, m)
+	err = GenerateOutput(config.OutputFile, transactions)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func main() {
+	configPtr := flag.String("config", "config.yaml", "The config file to use")
+	accountPtr := flag.String("a", "Assets:EE:Bank:Personal:Checking", "The default beancount account to use for the CSV transactions")
+	verbosePtr := flag.Bool("v", false, "Set logging to Debug level")
+
+	flag.Parse()
+
+	if *verbosePtr {
+		log.SetLevel(log.DebugLevel)
+	}
+
+	acc := models.Account{Name: *accountPtr}
+	c, err := models.ReadConfig(*configPtr)
+	if err != nil {
+		log.Error(err)
+		return
+	}
+	err = Run(c, acc)
+
+	if err != nil {
+		log.Error(err)
+	}
+}
---
+++ utils/utils.go
@@ -0,0 +1,63 @@
+package utils
+
+import (
+	"bufio"
+	"encoding/csv"
+	"errors"
+	"os"
+	"regexp"
+
+	log "github.com/sirupsen/logrus"
+	"github.com/sudneo/csv2beancount/models"
+)
+
+func GetAccounts(BeancountLedger string) []models.Account {
+	var accounts []models.Account
+	r := regexp.MustCompile("open (?P<account>[a-zA-Z]+(:([a-zA-Z]*))*)")
+	file, err := os.Open(BeancountLedger)
+	if err != nil {
+		log.WithFields(log.Fields{
+			"Error": err,
+			"File":  BeancountLedger,
+		}).Error("Failed to read Beancount file to source accounts")
+	}
+	scanner := bufio.NewScanner(file)
+	scanner.Split(bufio.ScanLines)
+
+	for scanner.Scan() {
+		line := scanner.Text()
+		matches := r.FindStringSubmatch(line)
+		if matches == nil {
+			continue
+		} else {
+			accountIndex := r.SubexpIndex("account")
+			a := models.Account{Name: matches[accountIndex]}
+			accounts = append(accounts, a)
+		}
+	}
+	return accounts
+}
+
+func ReadCsvFile(filePath string) ([][]string, error) {
+	f, err := os.Open(filePath)
+	if err != nil {
+		log.WithFields(log.Fields{
+			"File":  filePath,
+			"Error": err,
+		}).Error("Unable to read input file")
+		return [][]string{}, errors.New("Failed to read CSV")
+	}
+	defer f.Close()
+
+	csvReader := csv.NewReader(f)
+	csvReader.Comma = ';'
+	records, err := csvReader.ReadAll()
+	if err != nil {
+		log.WithFields(log.Fields{
+			"File":  filePath,
+			"Error": err,
+		}).Error("Unable to read input file")
+		return [][]string{}, errors.New("Failed to parse CSV")
+	}
+	return records, nil
+}
